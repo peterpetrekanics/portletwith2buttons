aa3bc25b8c03498065592b6bd24a3e46e7a86e0b
petrep <petrep@freemail.hu>
2016-04-01 20:05:44 +0200

    Added 2 buttons, and the portlet is now capable of: - showing which button was clicked

--- docroot/WEB-INF/src/com/test/Portletwith2buttons.java
+++ docroot/WEB-INF/src/com/test/Portletwith2buttons.java
@@ -1,6 +1,13 @@
 package com.test;
 
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
+
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
 
 /**
  * Portlet implementation class Portletwith2buttons
@@ -8,4 +15,14 @@ import com.liferay.util.bridges.mvc.MVCPortlet;
 public class Portletwith2buttons extends MVCPortlet {
  
 
+	 public void javaActionMethod(ActionRequest actionRequest,
+			 ActionResponse actionResponse) throws IOException, PortletException {
+		 		System.out.println("Testing starts..");
+
+		 		String buttonValue=ParamUtil.get(actionRequest, "button1", "");
+		 		System.out.println("The following button was pressed: " + buttonValue);
+		 		
+		 		System.out.println("Testing ends..");
+	 }
+	 
 }
--- docroot/html/portletwith2buttons/view.jsp
+++ docroot/html/portletwith2buttons/view.jsp
@@ -3,3 +3,12 @@
 <portlet:defineObjects />
 
 This is the <b>Portletwith2buttons</b> portlet in View mode.
+
+<portlet:actionURL var="buttonActionURL" windowState="normal"
+name="javaActionMethod">
+</portlet:actionURL>
+
+ <form action="<%=buttonActionURL%>" name="buttonPressed"  method="POST">
+	<input type="submit" name="button1" id="buttonID1" value="Button 1"/>
+	<input type="submit" name="button1" id="buttonID2" value="Button 2"/>
+</form>
