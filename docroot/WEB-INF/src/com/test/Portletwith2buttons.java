0a5129d14b04bb029645ab1ee230ce085df11eed
petrep <petrep@freemail.hu>
2016-04-01 19:17:29 +0200

    initial commit

---
+++ docroot/WEB-INF/src/com/test/Portletwith2buttons.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class Portletwith2buttons
+ */
+public class Portletwith2buttons extends MVCPortlet {
+ 
+
+}
